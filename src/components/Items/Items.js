f922f24a8318760fb289ae645a5294d149d9f1fb
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-04-30 08:55:23 +0600

    items section title changed

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -9,7 +9,7 @@ const Items = () => {
           <div class="container px-5 py-24 mx-auto flex flex-wrap">
             <div class="flex w-full mb-20 flex-wrap">
               <h1 class="sm:text-3xl text-2xl font-medium title-font text-white lg:w-1/3 lg:mb-0 mb-4">
-                Perfumes Collection in Stock
+                Comming Soon in Stock
               </h1>
               <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">
                 Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
