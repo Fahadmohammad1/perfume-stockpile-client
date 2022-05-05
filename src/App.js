d2b25cdac7fa63aa6d8ef62c5ed6b536863cf7a5
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-05-05 23:28:09 +0600

    login form added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Home></Home>
+      <Routes>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
